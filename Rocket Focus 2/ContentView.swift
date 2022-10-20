a656cd304a77d7f4218c34d0c8c998b662a26e77
Yogesh Nikash Ramsorrun <specineff00@gmail.com>
2022-10-20 09:44:12 +0100

    Initial Commit

---
+++ Rocket Focus 2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rocket Focus 2
+//
+//  Created by Yogesh Nikash Ramsorrun on 20/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rocket Focus 2/Rocket_Focus_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Rocket_Focus_2App.swift
+//  Rocket Focus 2
+//
+//  Created by Yogesh Nikash Ramsorrun on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Rocket_Focus_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
